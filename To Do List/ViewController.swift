d9620e1dbc6ff47d7938cee2406e173a30a4ceef
Frank Brammer <fbrammer@brammeris.com>
2018-04-09 13:54:24 -0400

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Frank Brammer on 4/9/18.
+//  Copyright © 2018 BrammerIS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
